a844103fca7407f09bfdde933f07312e3b280b77
Eric Myoung Hun Wang <Eric@Erics-Macbook-Pro.local>
2020-02-16 14:51:53 -0500

    Completed functionality for displaying all the items and the amounts

--- js/app.js
+++ js/app.js
@@ -64,11 +64,69 @@ class UI {
         self.expenseFeedback.classList.remove('showItem');
       }, 4000)
     }
+    else {
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = '';
+      this.amountInput.value = '';
+      // expense object to add it to the list in UI class
+      let expense = {
+        id:this.itemID,
+        title:expenseValue,
+        amount:amount,
+      }
+      this.itemID++
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
   }
+
+  // add expense
+  addExpense(expense) {
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+    `;
+    this.expenseList.appendChild(div);
+  }
+
   // total expense
   totalExpense() {
-    let total = 400;
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce(function(acc, curr) {
+        console.log(`Total is: ${acc} and the current value is: ${curr.amount}`);
+        acc += curr.amount;
+        return acc;
+      }, 0);
+    }
+    this.expenseAmount.textContent = total;
     return total;
+  }
+
+  // edit expense
+  editExpense(element) {
+
+  }
+
+  // delete expense
+  deleteExpense(element) {
+
   }
 }
 
@@ -93,8 +151,13 @@ function eventListeners() {
   });
 
   // expense click
-  expenseList.addEventListener('click', function(){
-    
+  expenseList.addEventListener('click', function(event){
+    if (event.target.parentElement.classList.contains('edit-icon')) {
+      ui.editExpense(event.target.parentElement);
+    }
+    else if (event.target.parentElement.classList.contains('delete-icon')) {
+      ui.deleteExpense(event.target.parentElement);
+    }
   });
 }
 
