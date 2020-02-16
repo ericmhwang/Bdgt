d89688a53f9e9d4ae9a39a2f98a3bbc47c80f77e
Eric Myoung Hun Wang <Eric@Erics-Macbook-Pro.local>
2020-02-16 12:19:59 -0500

    eventListners and showItem

--- js/app.js
+++ js/app.js
@@ -15,4 +15,44 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  // submit budget method
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if (value === '' || value < 0) {
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = '<p>value cannot be empty or negative</p>';
+    }
+  }
 }
+
+function eventListeners() {
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  // new instance of UI class
+  const ui = new UI();
+
+  // budget form submit event
+  budgetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+  });
+
+  // expense form submit event
+  expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+  });
+
+  // expense click
+  expenseList.addEventListener('click', function(){
+    
+  });
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  eventListeners();
+});
